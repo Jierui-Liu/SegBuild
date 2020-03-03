7e3e3776567b01ff8790e78e8852d72c084baf0a
lin-honghui <lin.honghui@qq.com>
2020-03-03 12:15:20 -0500

    add SegBuild.yaml

--- inference.sh
+++ inference.sh
@@ -2,7 +2,7 @@
  # @Author      : now more
  # @Contact     : lin.honghui@qq.com
  # @LastEditors: Please set LastEditors
- # @LastEditTime: 2020-03-01 21:19:16
+ # @LastEditTime: 2020-03-03 08:41:39
  # @Description : 
  ###
 # >>> conda initialize >>>
@@ -28,6 +28,7 @@ config_file="../configs/server_6121_hrnet_w48_up8_freeze_9_sgd_cross_entropy2d_t
 val_image_dir="/home/LinHonghui/Datasets/SegBulid/test_dataset_npy/image_bin/"
 # path="/home/LinHonghui/Project/DrivenData_2020_SegBulid/output/model/hrnet_w48_up8_server_6121_hrnet_w48_up8_freeze_9_sgd_cross_entropy2d_train_1_batch_12_step_size_up_5000_40000.pth"
 # path="/home/LinHonghui/Project/DrivenData_2020_SegBulid/output/model/hrnet_w48_up8_server_6121_hrnet_w48_up8_freeze_9_sgd_cross_entropy2d_train_1_agri_batch_16_step_size_up_2500_30000.pth" # 78.60
-path="/home/LinHonghui/Project/DrivenData_2020_SegBulid/output/model/hrnet_w48_up8_server_6121_hrnet_w48_up8_freeze_9_sgd_cross_entropy2d_train_1_agri_batch_16_step_size_up_2500_25000.pth"
-python inference.py -device 0 1 2  -path $path -val_batch_size 24 -config_file $config_file \
+# path="/home/LinHonghui/Project/DrivenData_2020_SegBulid/output/model/hrnet_w48_up8_server_6121_hrnet_w48_up8_freeze_9_sgd_cross_entropy2d_train_1_agri_batch_16_step_size_up_2500_25000.pth" #7860
+path="/home/LinHonghui/Project/DrivenData_2020_SegBulid/output/model/hrnet_w48_up8_server_6125_hrnet_w48_up8_freeze_9_sgd_cross_entropy2d_train_1_agri_p79_batch_12_step_size_up_5000_20000.pth"
+python inference.py -device 0 1 2 3 -path $path -val_batch_size 24 -config_file $config_file \
     -val_image_dir $val_image_dir
